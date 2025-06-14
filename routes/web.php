82d715efcbd193ad50f3961bac4af9db8a2be659
DariaNovak <daria.novak@oa.edu.ua>
2025-06-14 22:55:55 +0300

    Lab 13: Add video processing and catalog preparation functionality: implement processVideo and prepareCatalog methods in DiggingDeeperController, and create new job classes for catalog generation.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,19 @@
 <?php
-use App\Http\Controllers\RestTestController;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DiggingDeeperController;
+use App\Http\Controllers\Blog\Admin\CategoryController;
+use App\Http\Controllers\Blog\Admin\PostController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group([
+    'namespace' => 'App\Http\Controllers\Blog',
+    'prefix' => 'blog'
+], function () {
+    Route::resource('posts', PostController::class)->names('blog.posts');
 });
 
 Route::middleware([
@@ -17,28 +26,32 @@ Route::middleware([
     })->name('dashboard');
 });
 
-Route::resource('rest', RestTestController::class)->names('restTest');
-Route::group(['namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
-    Route::resource('posts', PostController::class)->names('blog.posts');
-});
-
 //Адмінка
 $groupData = [
     'namespace' => 'App\Http\Controllers\Blog\Admin',
     'prefix' => 'admin/blog',
 ];
+
 Route::group($groupData, function () {
     //BlogCategory
-    $methods = ['index','edit','store','update','create',];
+    $methods = ['index','edit','store','update','create'];
     Route::resource('categories', CategoryController::class)
-    ->only($methods)
-    ->names('blog.admin.categories'); 
-      //BlogPost
+        ->only($methods)
+        ->names('blog.admin.categories');
+    
+    //BlogPost
     Route::resource('posts', PostController::class)
-        ->except(['show'])                               //не робити маршрут для метода show
+        ->except(['show']) //не робити маршрут для метода show
         ->names('blog.admin.posts');
 });
+
 Route::group(['prefix' => 'digging_deeper'], function () {
     Route::get('collections', [DiggingDeeperController::class, 'collections'])
         ->name('digging_deeper.collections');
+
+    Route::get('process-video', [DiggingDeeperController::class, 'processVideo'])
+        ->name('digging_deeper.processVideo');
+        
+    Route::get('prepare-catalog', [DiggingDeeperController::class, 'prepareCatalog'])
+        ->name('digging_deeper.prepareCatalog');
 });
